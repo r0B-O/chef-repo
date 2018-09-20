51250c04e61d3b563d73f3af760ce59243d05105
Cloud User <centos@ip-10-124-1-134.ec2.internal>
2018-09-20 05:47:56 +0000

    Cookbook updated

--- cookbooks/bcf_nginx/recipes/default.rb
+++ cookbooks/bcf_nginx/recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+package "httpd" 
+
+service "httpd" do
+	action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+	content "<h1>Hello, World!</h1>"
+	action :create
+	not_if { ::File.exists?("/var/www/html/index.html") }
+end
